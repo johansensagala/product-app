fa0abbefddde1c3a2526024593e4c4c06505ac30
johansensagala <johansensagala.2001@gmail.com>
2024-10-17 11:01:53 +0700

    hapus setter getter ganti lombok

--- src/main/java/test_project/product_app/model/OrderCart.java
+++ src/main/java/test_project/product_app/model/OrderCart.java
@@ -16,19 +16,4 @@ public class OrderCart {
     @OneToMany(mappedBy = "orderCart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<OrderItem> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setOrderItems(List<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
 }
--- src/main/java/test_project/product_app/model/OrderItem.java
+++ src/main/java/test_project/product_app/model/OrderItem.java
@@ -21,36 +21,4 @@ public class OrderItem {
 
     private int quantity;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public OrderCart getOrderCart() {
-        return orderCart;
-    }
-
-    public void setOrderCart(OrderCart orderCart) {
-        this.orderCart = orderCart;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
 }
